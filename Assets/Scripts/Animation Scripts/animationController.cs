74da0cf54c4a5ecca58f8cdfc94b9a83c385bfc6
unknown <willroymyles@yahoo.com>
2018-10-18 14:43:10 -0500

    Added running animation

---
+++ Assets/Scripts/Animation Scripts/animationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationController : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var hori = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("right pressed");
+            anim.SetBool("running", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            Debug.Log("right pressed");
+            anim.SetBool("running", false);
+        }
+    }
+}
